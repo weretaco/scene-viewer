92125bd56320285a78edc92e33de7890d3e302ae
Dmitry Portnoy <dportnoy@andrew.cmu.edu>
2024-02-12 19:14:59 -0500

    Add a VS project to test CLI args

---
+++ Launcher.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World!\n" << std::endl;
+
+    // Parse CLI args into sections by "--* ...", separate each section into the flag name and
+    // the flag parameters, which can be passed as some sort of array
+    // to a handler function specific to each flag
+}
