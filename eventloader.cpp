5acaa4040ab562deb092f19857c641b56623b782
Dmitry Portnoy <dportnoy@andrew.cmu.edu>
2024-02-19 17:52:21 -0500

    Parse events

--- eventloader.cpp
+++ eventloader.cpp
@@ -11,15 +11,52 @@ EventLoader::EventLoader(std::string fileName) {
     }
 }
 
-void EventLoader::parseEvents() {
-    std::cout << "PARTY TIME" << std::endl;
+std::vector<EventLoader::Event> EventLoader::parseEvents() {
+    std::vector<EventLoader::Event> events;
 
     std::string line;
     while (std::getline(fileStream, line)) {
         std::cout << line << std::endl;
+
+        std::vector<std::string> params = splitLine(line);
+
+        events.push_back({});
+
+        events.back().timestamp = stol(params[0]);
+
+        if (params[1] == "AVAILABLE") {
+            events.back().type = Event::Type::AVAILABLE;
+        } else if (params[1] == "PLAY") {
+            events.back().type = Event::Type::PLAY;
+        } else if (params[1] == "SAVE") {
+            events.back().type = Event::Type::SAVE;
+        } else if (params[1] == "MARK") {
+            events.back().type = Event::Type::MARK;
+        } else {
+            throw std::runtime_error("Unexpected event type: " + params[1]);
+        }
+
+        for (int i = 2; i < params.size(); i++) {
+            events.back().args.push_back(params[i]);
+        }
     }
+
+    return events;
 }
 
 void EventLoader::close() {
     fileStream.close();
+}
+
+std::vector<std::string> EventLoader::splitLine(const std::string& line) {
+    std::vector<std::string> params;
+
+    std::string param;
+    std::stringstream ss{line};
+
+    while (std::getline(ss, param, ' ')) {
+        params.push_back(param);
+    }
+
+    return params;
 }
--- eventloader.h
+++ eventloader.h
@@ -2,19 +2,35 @@
 #define _EVENT_LOADER_H
 
 #include <fstream>
+#include <sstream>
 #include <vector>
 
 class EventLoader {
 public:
+
+    struct Event {
+        enum class Type {
+            AVAILABLE,
+            PLAY,
+            SAVE,
+            MARK
+        };
+        uint32_t timestamp;
+        Type type;
+        std::vector<std::string> args;
+    };
+
     EventLoader(std::string filename);
 
-    void parseEvents();
+    std::vector<EventLoader::Event> parseEvents();
 
     void close();
 
 private:
 
     std::fstream fileStream;
+
+    std::vector<std::string> splitLine(const std::string& line);
 };
 
 #endif // _JSON_LOADER_H
