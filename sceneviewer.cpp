5ef23282fd8af0b2265eda8920ef25b902983423
Dmitry Portnoy <dportnoy@medievaltech.com>
2024-02-22 23:05:06 -0500

    Improve error reporting

--- sceneviewer.cpp
+++ sceneviewer.cpp
@@ -489,7 +489,7 @@ private:
         std::ifstream file(filename, std::ios::ate | std::ios::binary);
 
         if (!file.is_open()) {
-            throw std::runtime_error("failed to open file!");
+            throw std::runtime_error("failed to open " + filename + "!");
         }
 
         size_t fileSize = (size_t)file.tellg();
